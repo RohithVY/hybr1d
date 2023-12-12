abe8c8f210aec5008d899337ed582bfd530a934d
RohithVY <venkatrohith1998@gmail.com>
2023-12-12 19:19:36 +0530

    Added break word property

--- src/components/NewsCardComments.tsx
+++ src/components/NewsCardComments.tsx
@@ -30,7 +30,7 @@ const NewsCardComments = ({ comments }: Props) => {
                 <Markdown
                   options={{ forceBlock: true }}
                   key={eachComment.id + eachComment.created_at}
-                  className="flex flex-col gap-2 text-sm  text-[#acadb0] px-4 py-2 "
+                  className="flex flex-col gap-2 text-sm break-words text-[#acadb0] px-4 py-2 "
                 >
                   {eachComment.text}
                 </Markdown>
